ab7ddc683492d801cd4990868faf9077024c342a
stvdeshon <stvdeshon@gmail.com>
2023-02-23 16:06:36 -0700

    Remove extraneous code

--- src/board-builder.js
+++ src/board-builder.js
@@ -25,20 +25,6 @@ export default class BoardBuilder {
     parent.insertAdjacentHTML("afterbegin", matrix);
   }
 
-  renderFleet(board) {
-    const shipContainer = document.getElementById("ships");
-    for (let i = 0; i < board.fleet.length; i++) {
-      let ship = board.fleet[i];
-      shipContainer.classList.add(`${ship.name}-docked`);
-      shipContainer.setAttribute("draggable", true);
-      shipContainer.dataset.ship = `${ship.name}`;
-      for (let j = 0; j < ship.length; i++) {
-        shipContainer.innerHTML += `<div class=${ship.name} data-index='${i}'></div>`;
-      }
-      elements.fleetDraggable.prepend(shipContainer); //imported from an elements object
-    }
-  }
-
   resetBoard(parent) {
     parent.textContent = "";
   }
